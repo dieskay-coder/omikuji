2e5e86459701c0f53d34b752c33516751efc14fe
dieskay-coder <66352779+dieskay-coder@users.noreply.github.com>
2021-04-29 17:00:32 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 // 基本的な作り方(Math.floor)
 // {
 //   // IDにより要素を取得
@@ -33,22 +32,36 @@
 // 確率を操作して作る(Math.random)
 {
   const btn = document.getElementById('btn');
+  const sound1 = new Audio("fanfare.mp3"); 
+  const sound2 = new Audio("pafu.mp3");
+  const sound3 = new Audio("chanchan.mp3");
 
   btn.addEventListener('click', () => {
-    const n = Math.random();
-    if (n < 0.2) {
-      btn.textContent = 'おおあたり!!';
-    }else if (n < 0.7) {
-      btn.textContent = 'あたり!';
-    }else {
-      btn.textContent ='はずれ(>o<)';
-    }
-  });
+    window.setTimeout(() => {
+      const n = Math.random();
+      if (n < 0.5) {
+        btn.textContent = 'おおあたり!!';
+        sound1.play();
+      }else if (n < 0.8) {
+        btn.textContent = 'あたり!';
+        sound2.play();
+      }else {
+        btn.textContent ='はずれ(>o<)';
+        sound3.play();
+      };
+    },4800);
+  },{once: true});
 }
 
 {
   const reload =document.getElementById('reload');
   reload.addEventListener('click',function(){
+    document.getElementById('reset').play();
     window.location.reload();
 });
+}
+{
+  document.getElementById('btn').onclick = function(){
+    document.getElementById('hiku').play();
+  };
 }
